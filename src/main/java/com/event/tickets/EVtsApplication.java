8bf71d67a8ab3d10a34ce773650fbfead74c8554
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-06-24 06:22:58 +0530

    project initial setup with lombok and mapstruct

---
+++ src/main/java/com/event/tickets/EVtsApplication.java
@@ -0,0 +1,13 @@
+package com.event.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( EVtsApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/event/tickets/EVtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
